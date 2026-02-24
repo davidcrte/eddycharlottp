2434f51f069404ff3216e3c75771438dbe8a54a2
davcrte <courte.david@gmail.com>
2026-02-24 22:07:41 +0100

    Configure Next.js and GitHub Actions for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let basePath = '';
+
+if (isGithubActions) {
+    // Trim off the @user/ part and keep only the repository name
+    const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+    basePath = `/${repo}`;
+}
+
 const nextConfig = {
     output: "export",
+    basePath: basePath,
+    assetPrefix: basePath,
     trailingSlash: true,
     images: {
         unoptimized: true,
